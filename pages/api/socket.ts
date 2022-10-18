777c5ef4ea16db27bce6ed53f5d39d8c1c733921
Josh <66529773+Joshuahuahua@users.noreply.github.com>
2022-10-18 19:57:32 +0800

    Added nanoId

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,5 +1,19 @@
-import { Server as ServerIO } from "socket.io";
+import { Server as ServerIO, Socket } from "socket.io";
 import { Server as HttpServer } from "http";
+import { nanoid } from 'nanoid'
+
+const EVENTS = {
+  connection: 'connection',
+  CLIENT: {
+    CREATE_ROOM: 'CREATE_ROOM',
+  },
+  SERVER: {
+    ROOMS: 'ROOMS',
+    JOINED_ROOM: 'JOINED_ROOM',
+  }
+}
+
+const rooms: Record<string, {name: string} > = {};
 
 const SocketHandler = async (req: any, res: any) => {
   if (!res.socket.server.io) {
@@ -9,6 +23,25 @@ const SocketHandler = async (req: any, res: any) => {
       path: '/api/socket'
     })
     res.socket.server.io = io;
+
+    io.on("connection", (socket: Socket) => {
+      socket.on(EVENTS.CLIENT.CREATE_ROOM, ({ roomName }) => {
+        console.log(roomName);
+        // gen a room id
+        const roomId = nanoid();
+        // add it to the pile
+        rooms[roomId] = { name: roomName };
+
+        socket.join(roomId);
+        
+        // "Yo, theres a new room!"
+        socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms)
+        // tell the room creater all the rooms
+        socket.emit(EVENTS.SERVER.ROOMS, rooms)
+        // tell the creater that he's joined the room
+        socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId)
+      });
+    });
   }
   res.end()
 };
